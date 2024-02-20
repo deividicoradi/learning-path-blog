fddcd7ce97ad96cdc4156b66a5a45a5eb6d7b96f
deividicoradi <deividi.coradii@gmail.com>
2024-02-20 10:09:32 -0300

    Add tests for Blog posts published methods

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Draft? Returns true for draft blog post" do
+    assert blog_posts(:draft).draft?
+  end
+
+  test "Draft? Returns false for published blog post" do
+    refute blog_posts(:published).draft?
+  end
+
+  test "Draft? Returns false for scheduled blog post" do
+    refute blog_posts(:scheduled).draft?
+  end
+  test "Published? Returns true for published blog post" do
+    assert blog_posts(:published).published?
+  end
+
+  test "Published? Returns false for draft blog post" do
+    refute blog_posts(:draft).published?
+  end
+
+  test "Published? Returns false for scheduled blog post" do
+    refute blog_posts(:scheduled).published?
+  end
+
+  test "Scheduled? Returns true for scheduled blog post" do
+    assert blog_posts(:scheduled).scheduled?
+  end
+
+  test "Scheduled? Returns false for draft blog post" do
+    refute blog_posts(:draft).scheduled?
+  end
+
+  test "Scheduled? Returns false for draft published post" do
+    refute blog_posts(:published).scheduled?
+  end
 end
